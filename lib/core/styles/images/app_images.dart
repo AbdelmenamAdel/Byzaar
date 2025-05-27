9c625bfbe3fa6651191d5af84545bdc1cc343ca4
Abdelmoneim Adel <abdelmenamadel96@gmail.com>
2025-05-27 03:03:20 +0300

    update image generator and add app strings file

--- lib/core/styles/images/app_images.dart
+++ lib/core/styles/images/app_images.dart
@@ -1,4 +1,33 @@
+// ignore_for_file: prefer_single_quotes
 class AppImages {
-  static const String pageUnderBuild = 'assets/images/page_under_build.png';
-  static const String wifiOff = 'assets/images/wifi_off.gif';
+  AppImages._();
+  
+  /// Assets for appIcon
+  /// assets/images/app_icon.png
+  static const String appIcon = "assets/images/app_icon.png";
+
+  /// Assets for appIcon50
+  /// assets/images/app_icon_50.png
+  static const String appIcon50 = "assets/images/app_icon_50.png";
+
+  /// Assets for byzaarDark
+  /// assets/images/Byzaar_dark.png
+  static const String byzaarDark = "assets/images/Byzaar_dark.png";
+
+  /// Assets for byzaarLight
+  /// assets/images/Byzaar_light.png
+  static const String byzaarLight = "assets/images/Byzaar_light.png";
+
+  /// Assets for emptyScreen
+  /// assets/images/empty_screen.png
+  static const String emptyScreen = "assets/images/empty_screen.png";
+
+  /// Assets for pageUnderBuild
+  /// assets/images/page_under_build.jpg
+  static const String pageUnderBuild = "assets/images/page_under_build.jpg";
+
+  /// Assets for wifiOff
+  /// assets/images/wifi_off.gif
+  static const String wifiOff = "assets/images/wifi_off.gif";
 }
+
