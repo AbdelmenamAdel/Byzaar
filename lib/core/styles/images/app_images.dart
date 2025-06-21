5ea2c25a523dde22fe8960b46fefd21de7c960af
Abdelmoneim Adel <abdelmenamadel96@gmail.com>
2025-06-21 20:03:19 +0300

    app styles

--- lib/core/styles/images/app_images.dart
+++ lib/core/styles/images/app_images.dart
@@ -1,33 +1,153 @@
 // ignore_for_file: prefer_single_quotes
-class AppImages {
-  AppImages._();
+class Assets {
+  Assets._();
   
-  /// Assets for appIcon
-  /// assets/images/app_icon.png
-  static const String appIcon = "assets/images/app_icon.png";
+  /// Assets for imagesAppIcon
+  /// assets/images/app_icon_.png
+  static const String imagesAppIcon = "assets/images/app_icon_.png";
 
-  /// Assets for appIcon50
+  /// Assets for imagesAppIcon50
   /// assets/images/app_icon_50.png
-  static const String appIcon50 = "assets/images/app_icon_50.png";
+  static const String imagesAppIcon50 = "assets/images/app_icon_50.png";
 
-  /// Assets for byzaarDark
+  /// Assets for imagesApplIcon
+  /// assets/images/appl_icon.svg
+  static const String imagesApplIcon = "assets/images/appl_icon.svg";
+
+  /// Assets for imagesByzaarDark
   /// assets/images/Byzaar_dark.png
-  static const String byzaarDark = "assets/images/Byzaar_dark.png";
+  static const String imagesByzaarDark = "assets/images/Byzaar_dark.png";
 
-  /// Assets for byzaarLight
+  /// Assets for imagesByzaarLight
   /// assets/images/Byzaar_light.png
-  static const String byzaarLight = "assets/images/Byzaar_light.png";
+  static const String imagesByzaarLight = "assets/images/Byzaar_light.png";
 
-  /// Assets for emptyScreen
+  /// Assets for imagesCheck
+  /// assets/images/Check.svg
+  static const String imagesCheck = "assets/images/Check.svg";
+
+  /// Assets for imagesEdit
+  /// assets/images/edit.svg
+  static const String imagesEdit = "assets/images/edit.svg";
+
+  /// Assets for imagesEmptyScreen
   /// assets/images/empty_screen.png
-  static const String emptyScreen = "assets/images/empty_screen.png";
+  static const String imagesEmptyScreen = "assets/images/empty_screen.png";
 
-  /// Assets for pageUnderBuild
+  /// Assets for imagesFacebookIcon
+  /// assets/images/facebook_icon.svg
+  static const String imagesFacebookIcon = "assets/images/facebook_icon.svg";
+
+  /// Assets for imagesFeaturedItemBackground
+  /// assets/images/featured_item_background.svg
+  static const String imagesFeaturedItemBackground = "assets/images/featured_item_background.svg";
+
+  /// Assets for imagesFilter
+  /// assets/images/filter.svg
+  static const String imagesFilter = "assets/images/filter.svg";
+
+  /// Assets for imagesFilter2
+  /// assets/images/filter2.svg
+  static const String imagesFilter2 = "assets/images/filter2.svg";
+
+  /// Assets for imagesGoogleIcon
+  /// assets/images/google_icon.svg
+  static const String imagesGoogleIcon = "assets/images/google_icon.svg";
+
+  /// Assets for imagesLocation
+  /// assets/images/location.svg
+  static const String imagesLocation = "assets/images/location.svg";
+
+  /// Assets for imagesLogo
+  /// assets/images/logo.svg
+  static const String imagesLogo = "assets/images/logo.svg";
+
+  /// Assets for imagesNotification
+  /// assets/images/notification.svg
+  static const String imagesNotification = "assets/images/notification.svg";
+
+  /// Assets for imagesPageUnderBuild
   /// assets/images/page_under_build.jpg
-  static const String pageUnderBuild = "assets/images/page_under_build.jpg";
+  static const String imagesPageUnderBuild = "assets/images/page_under_build.jpg";
 
-  /// Assets for wifiOff
+  /// Assets for imagesPageViewItem1BackgroundImage
+  /// assets/images/page_view_item1_background_image.svg
+  static const String imagesPageViewItem1BackgroundImage = "assets/images/page_view_item1_background_image.svg";
+
+  /// Assets for imagesPageViewItem1Image
+  /// assets/images/page_view_item1_image.svg
+  static const String imagesPageViewItem1Image = "assets/images/page_view_item1_image.svg";
+
+  /// Assets for imagesPageViewItem2BackgroundImage
+  /// assets/images/page_view_item2_background_image.svg
+  static const String imagesPageViewItem2BackgroundImage = "assets/images/page_view_item2_background_image.svg";
+
+  /// Assets for imagesPageViewItem2Image
+  /// assets/images/page_view_item2_image.svg
+  static const String imagesPageViewItem2Image = "assets/images/page_view_item2_image.svg";
+
+  /// Assets for imagesPlant
+  /// assets/images/plant.svg
+  static const String imagesPlant = "assets/images/plant.svg";
+
+  /// Assets for imagesProfileImage
+  /// assets/images/profile_image.png
+  static const String imagesProfileImage = "assets/images/profile_image.png";
+
+  /// Assets for imagesSearchIcon
+  /// assets/images/search_icon.svg
+  static const String imagesSearchIcon = "assets/images/search_icon.svg";
+
+  /// Assets for imagesSplashBottom
+  /// assets/images/splash_bottom.svg
+  static const String imagesSplashBottom = "assets/images/splash_bottom.svg";
+
+  /// Assets for imagesTrash
+  /// assets/images/trash.svg
+  static const String imagesTrash = "assets/images/trash.svg";
+
+  /// Assets for imagesVector
+  /// assets/images/Vector.svg
+  static const String imagesVector = "assets/images/Vector.svg";
+
+  /// Assets for imagesVuesaxBoldHome
+  /// assets/images/vuesax/bold/home.svg
+  static const String imagesVuesaxBoldHome = "assets/images/vuesax/bold/home.svg";
+
+  /// Assets for imagesVuesaxBoldProducts
+  /// assets/images/vuesax/bold/products.svg
+  static const String imagesVuesaxBoldProducts = "assets/images/vuesax/bold/products.svg";
+
+  /// Assets for imagesVuesaxBoldShoppingCart
+  /// assets/images/vuesax/bold/shopping-cart.svg
+  static const String imagesVuesaxBoldShoppingCart = "assets/images/vuesax/bold/shopping-cart.svg";
+
+  /// Assets for imagesVuesaxBoldUser
+  /// assets/images/vuesax/bold/user.svg
+  static const String imagesVuesaxBoldUser = "assets/images/vuesax/bold/user.svg";
+
+  /// Assets for imagesVuesaxOutlineHome
+  /// assets/images/vuesax/outline/home.svg
+  static const String imagesVuesaxOutlineHome = "assets/images/vuesax/outline/home.svg";
+
+  /// Assets for imagesVuesaxOutlineProducts
+  /// assets/images/vuesax/outline/products.svg
+  static const String imagesVuesaxOutlineProducts = "assets/images/vuesax/outline/products.svg";
+
+  /// Assets for imagesVuesaxOutlineShoppingCart
+  /// assets/images/vuesax/outline/shopping-cart.svg
+  static const String imagesVuesaxOutlineShoppingCart = "assets/images/vuesax/outline/shopping-cart.svg";
+
+  /// Assets for imagesVuesaxOutlineUser
+  /// assets/images/vuesax/outline/user.svg
+  static const String imagesVuesaxOutlineUser = "assets/images/vuesax/outline/user.svg";
+
+  /// Assets for imagesWatermelonTest
+  /// assets/images/watermelon_test.png
+  static const String imagesWatermelonTest = "assets/images/watermelon_test.png";
+
+  /// Assets for imagesWifiOff
   /// assets/images/wifi_off.gif
-  static const String wifiOff = "assets/images/wifi_off.gif";
+  static const String imagesWifiOff = "assets/images/wifi_off.gif";
 }
 
