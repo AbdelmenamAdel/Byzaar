4c68409aa0435624a80c69da3c0b643c98cb2edb
Abdelmoneim Adel <abdelmenamadel96@gmail.com>
2025-06-21 20:03:28 +0300

    di

--- lib/core/dependancy_injection/service_locator.dart
+++ lib/core/dependancy_injection/service_locator.dart
@@ -1,10 +1,14 @@
 import 'package:byzaar/core/services/local/local_storage.dart';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:get_it/get_it.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 final sl = GetIt.instance;
 
 Future<void> setupInjector() async {
-  sl.registerFactory(
-      () => LocalStorage(secureStorage: const FlutterSecureStorage()));
+  sl.registerSingletonAsync<LocalStorage>(() async {
+    final prefs = await SharedPreferences.getInstance();
+    return LocalStorage(sharedInstance: prefs);
+  });
+
+  await sl.allReady();
 }
