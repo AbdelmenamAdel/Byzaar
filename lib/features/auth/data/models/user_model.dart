3727adbb1253441aa41fed5262430d2aeec43f9d
Abdelmoneim Adel <abdelmenamadel96@gmail.com>
2025-06-28 03:36:36 +0300

    user model

--- lib/features/auth/data/models/user_model.dart
+++ lib/features/auth/data/models/user_model.dart
@@ -1,38 +1,38 @@
-// import 'package:firebase_auth/firebase_auth.dart';
-// import 'package:byzaar/features/auth/domain/entites/user_entity.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:fruit_hub/features/auth/domain/entites/user_entity.dart';
 
-// class UserModel extends UserEntity {
-//   UserModel({required super.name, required super.email, required super.uId});
+class UserModel extends UserEntity {
+  UserModel({required super.name, required super.email, required super.uId});
 
-//   factory UserModel.fromFirebaseUser(User user) {
-//     return UserModel(
-//       name: user.displayName ?? '',
-//       email: user.email ?? '',
-//       uId: user.uid,
-//     );
-//   }
+  factory UserModel.fromFirebaseUser(User user) {
+    return UserModel(
+      name: user.displayName ?? '',
+      email: user.email ?? '',
+      uId: user.uid,
+    );
+  }
 
-//   factory UserModel.fromJson(Map<String, dynamic> json) {
-//     return UserModel(
-//       name: json['name'],
-//       email: json['email'],
-//       uId: json['uId'],
-//     );
-//   }
+  factory UserModel.fromJson(Map<String, dynamic> json) {
+    return UserModel(
+      name: json['name'],
+      email: json['email'],
+      uId: json['uId'],
+    );
+  }
 
-//   factory UserModel.fromEntity(UserEntity user) {
-//     return UserModel(
-//       name: user.name,
-//       email: user.email,
-//       uId: user.uId,
-//     );
-//   }
+  factory UserModel.fromEntity(UserEntity user) {
+    return UserModel(
+      name: user.name,
+      email: user.email,
+      uId: user.uId,
+    );
+  }
 
-//   toMap() {
-//     return {
-//       'name': name,
-//       'email': email,
-//       'uId': uId,
-//     };
-//   }
-// }
+  toMap() {
+    return {
+      'name': name,
+      'email': email,
+      'uId': uId,
+    };
+  }
+}
