01bc4fec00d3b4b253322afaf6e1ec56cfb348fd
Abdelmoneim Adel <abdelmenamadel96@gmail.com>
2025-06-29 01:02:19 +0300

    add color on success

--- lib/core/widgets/custom_snackbar.dart
+++ lib/core/widgets/custom_snackbar.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 
-void showBar(BuildContext context, String message) {
+void showBar(BuildContext context, String message, {Color? color}) {
   ScaffoldMessenger.of(context).showSnackBar(
     SnackBar(
+      backgroundColor: color,
       behavior: SnackBarBehavior.floating,
       duration: const Duration(seconds: 1),
       content: Text(message),
